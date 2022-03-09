34c83dcb5ba1e3f97774eb759857fed7212c0289
Simo-C3 <shimomae.hitoshi210@mail.kyutech.jp>
2022-03-09 23:12:52 +0900

    [add] 独自の例外を定義

---
+++ exception.py
@@ -0,0 +1,15 @@
+class RoomCreateException(Exception):
+    """ルームの作成中にエラーが発生したことを知らせる例外クラス"""
+    pass
+
+class JoinUserException(Exception):
+    """ユーザーの参加中にエラーが発生したことを知らせる例外クラス"""
+    pass
+
+class ExitUserException(Exception):
+    """ユーザーの退出中にエラーが発生したことを知らせる例外クラス"""
+    pass
+
+class DeleteRoomException(Exception):
+    """ルームの削除中にエラーが発生したことを知らせる例外クラス"""
+    pass
